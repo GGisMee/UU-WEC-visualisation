745f482c5590187f51ccbfdd85d8c5bdeac47a76
gustavg <gustav.gamstedt@outlook.com>
2026-06-04 17:10:23 +0200

    most of energy calculations

--- Code/energy_calculations.py
+++ Code/energy_calculations.py
@@ -75,15 +75,58 @@ class EnergyCalculations:
             np.log(10/self.z0)
         ) # From formula
 
+
+
     def calculate(self):
         h = 1 # step value. Increase for better resolution
         wind_speeds = np.arange(1, 61, h)
 
-        # C constant gives characteristic windspeed 
+
+        # Calculate distrobution
         k = self.input_data.k_factor
-        C = self.wind_nacelle/ gamma(1+1/k)
-        f_weibull = (k/C)* (wind_speeds/C)**(k-1)*np.exp(-(wind_speeds/C)**k)
-        print(f_weibull[0:5])
+        C = self.wind_nacelle/ gamma(1+1/k) # C constant gives characteristic windspeed 
+        distrobution= (k/C)* (wind_speeds/C)**(k-1)*np.exp(-(wind_speeds/C)**k)
+
+        # Hours of the year
+        possible_hours = distrobution*8760
+        availability = (100-self.input_data.downtime)/100
+        available_hours = availability*possible_hours
+
+        # Energies
+        energy_per_m2 = 0.62*wind_speeds**3*possible_hours/1000 # KWh/m^2
+        tot_possible_energy_per_m2 = np.sum(energy_per_m2)
+        cumulated_energy = np.cumsum(energy_per_m2)
+
+
+        # Steps below to cap cumulated energy to simulate effect of Benz law
+        # Finds first index where cumulated_energy surpasses limit 
+        effective_limit = tot_possible_energy_per_m2/3
+        rated_mask = cumulated_energy > effective_limit
+        idx = np.argmax(rated_mask) 
+        cumulated_energy_rated = cumulated_energy.copy()
+        rated_velocity = wind_speeds.copy() # Effective windspeeds        
+        if cumulated_energy[idx] > effective_limit:
+            # Changes values after and at index to value before 
+            cumulated_energy_rated[idx:] = cumulated_energy_rated[idx] if idx > 0 else 0
+            rated_velocity[idx:] = rated_velocity[idx] if idx > 0 else 0
+            velocity_rated = rated_velocity[idx]
+
+        # capped by turn off limit
+        turn_off_limit = 0.8*tot_possible_energy_per_m2
+        capped_velocities = wind_speeds.copy() # array for velocities before WEC is turned off
+        capped_mask = cumulated_energy > turn_off_limit
+        idx = np.argmax(capped_mask)
+        self.cut_out = capped_velocities[idx]
+        if capped_mask[-1] != 0: # It shouldn't be all zeros
+            capped_velocities[idx:] = capped_velocities[idx]
+            velocity_cap = capped_velocities[idx]
+        
+        swept_area = np.pi*(self.input_data.diam/2)**2
+
+
+        
+        print(tot_possible_energy_per_m2)
+        
 
 
 if __name__ == "__main__":
