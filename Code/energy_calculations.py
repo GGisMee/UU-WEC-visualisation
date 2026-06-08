898e942e85c188df4117d6b532c9895367d0f01d
gustavg <gustav.gamstedt@outlook.com>
2026-06-08 10:32:36 +0200

    finished energy calculations

--- Code/energy_calculations.py
+++ Code/energy_calculations.py
@@ -104,29 +104,64 @@ class EnergyCalculations:
         rated_mask = cumulated_energy > effective_limit
         idx = np.argmax(rated_mask) 
         cumulated_energy_rated = cumulated_energy.copy()
-        rated_velocity = wind_speeds.copy() # Effective windspeeds        
+        rated_speeds = wind_speeds.copy() # Effective windspeeds        
         if cumulated_energy[idx] > effective_limit:
             # Changes values after and at index to value before 
             cumulated_energy_rated[idx:] = cumulated_energy_rated[idx] if idx > 0 else 0
-            rated_velocity[idx:] = rated_velocity[idx] if idx > 0 else 0
-            velocity_rated = rated_velocity[idx]
+            rated_speeds[idx:] = rated_speeds[idx] if idx > 0 else 0
+            rated_speed = rated_speeds[idx]
 
         # capped by turn off limit
         turn_off_limit = 0.8*tot_possible_energy_per_m2
         capped_velocities = wind_speeds.copy() # array for velocities before WEC is turned off
         capped_mask = cumulated_energy > turn_off_limit
         idx = np.argmax(capped_mask)
-        self.cut_out = capped_velocities[idx]
+        cut_out = capped_velocities[idx]
         if capped_mask[-1] != 0: # It shouldn't be all zeros
             capped_velocities[idx:] = capped_velocities[idx]
             velocity_cap = capped_velocities[idx]
         
         swept_area = np.pi*(self.input_data.diam/2)**2
 
+        cut_in = int(rated_speed * 0.01**(1/3)*10)/10
+        rated_power = 0.62*rated_speed**3*swept_area*self.input_data.capture_efficiency*self.input_data.efficiency_drivetrain/1000
 
-        
-        print(tot_possible_energy_per_m2)
-        
+
+
+        conditions = [
+            wind_speeds <= cut_in, # values below cut_in, therefore set to 0 below
+            (wind_speeds > cut_in) & (wind_speeds <= rated_speed), # variable energies
+            (wind_speeds > rated_speeds) & (wind_speeds <=cut_out), # Between rated and cut_out, therefore constant
+            wind_speeds >cut_out, # values above cut_out
+        ]
+
+        alternatives = [
+            0, # Shut off, zone 1
+            energy_per_m2*swept_area*self.input_data.capture_efficiency*self.input_data.efficiency_drivetrain, # zone 2
+            rated_power*available_hours, # zone 3, Since between rated and cutout. Energy output will just be rated_power*available_hours. Constant.
+            0, # zone 4, Shut off
+        ]
+
+        generated_energies = np.select(conditions, alternatives) # Energy for different windspeeds in kWh
+
+        generated_energy = np.sum(generated_energies)/1000 # Total energy per year in MWh
+
+        # Set values to remember
+        self.rated_speed = rated_speed
+        self.cut_out = cut_out
+        self.rated_power = rated_power
+        self.cut_in = cut_in
+        self.generated_energy = generated_energy # [MWh]
+        self.average_power = generated_energy/8.76 # [KWh], 8.76 from hours in year / 1000
+        self.full_load_hours = generated_energy/rated_power*1000 # [h], energy produced relative to maximum capacity
+        self.aerodynamical_load = 1/2*1.2*8/9*swept_area*rated_speed**2/1000
+        self.solidity = 3 #! Hur ändra denna? Ska den vara output?
+        self.storm_load = 1/2*1.2*1.5*self.solidity/100*swept_area*60**2/1000 #  @60 [kN], max at 60 kN. load under storm
+        self.wall_thickness_operation = self.aerodynamical_load*self.input_data.height/(np.pi*(self.input_data.height/40)**2*160)*2
+        self.wall_thickness_storm= self.storm_load*self.input_data.height/(np.pi*(self.input_data.height/40)**2*160)*2
+
+        print(self.rated_speed, self.cut_out,self.rated_power, self.cut_in,  self.generated_energy, self.average_power, self.full_load_hours,
+              self.aerodynamical_load, self.solidity, self.storm_load, self.wall_thickness_operation, self.wall_thickness_storm)
 
 
 if __name__ == "__main__":
